08e48cd996af8239c7c45fd5d1ed2c9b2f8b44e2
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-10-07 22:44:10 +0200

    Published as Version 0.0.2

--- test/platform_theming_test.dart
+++ test/platform_theming_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:platform_theming/platform_theming.dart';
 
 void main() {
-  test('adds one to input values', () {
-    expect(3, 3);
+  test('call constructor', () {
+    var theme = PlatformTheme();
   });
 }
